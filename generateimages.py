cd0963dcd876e39f0bc3207ce8a98ab4f695950c
Kris <kris.fillmore@gmail.com>
2021-04-19 17:57:32 -0700

    fixed bug where shutdown was occuring before all items were executed.

--- generateimages.py
+++ generateimages.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pandas as pd
 import ray
 
@@ -18,7 +20,17 @@ dfs = select_df(df, window_size)
 @ray.remote
 def execute_image_generator(df):
     GrammianImage(df, window_size, output_directory, "gasf", quality_pct).generateGaf()
-
-for df in dfs:
-    execute_image_generator.remote(df)
+print(len(dfs))
+ray_dfs = ray.put(dfs)
+#
+i_dfs = ray.get(ray_dfs)
+execute = [execute_image_generator.remote(df) for df in i_dfs]
+ready, not_ready = ray.wait(execute)
+ids = not_ready
+while len(not_ready) != 0:
+    ready, not_ready = ray.wait(ids)
+    print('Not Ready length:', len(not_ready))
+    ids = not_ready
+    if not ids:
+        break
 ray.shutdown()
